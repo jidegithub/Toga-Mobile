d87f2c8bbad65cc4a465e088b2712152ebcc9850
jidegithub <olutolajide@gmail.com>
2021-01-22 18:21:38 +0100

    fix/update: update logo size/fix mobile view navbar

--- src/partials/Header.js
+++ src/partials/Header.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import Toga from '../icons/Toga'
 
 function Header() {
+  const [isExpanded, toggleExpansion] = useState(false);
 
   const [top, setTop] = useState(true);
 
@@ -13,39 +14,51 @@ function Header() {
     };
     window.addEventListener('scroll', scrollHandler);
     return () => window.removeEventListener('scroll', scrollHandler);
-  }, [top]);  
+  }, [top]);
 
   return (
     <header className={`fixed w-full z-30 md:bg-opacity-90 transition duration-300 ease-in-out ${!top && 'bg-white blur shadow-lg'}`}>
       <div className="max-w-6xl mx-auto px-5 sm:px-6">
-        <div className="flex items-center justify-between h-16 md:h-20">
+        <div className="flex flex-wrap items-center justify-between max-w-4xl mx-auto p-4 md:p-8">
 
           {/* Site branding */}
-          <div className="flex-shrink-0 mr-4">
+          <div className="mr-4">
             {/* Logo */}
             <Link to="/" className="block" aria-label="toga">
-              <Toga/>
+              <Toga />
             </Link>
           </div>
 
+          <button
+            className="block md:hidden border border-blue flex items-center px-3 py-2 rounded text-blue"
+            onClick={() => toggleExpansion(!isExpanded)}
+          >
+            <svg
+              className="fill-current h-3 w-3"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <title>Menu</title>
+              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+            </svg>
+          </button>
+
           {/* Site navigation */}
-          <nav className="flex flex-grow">
+          <nav className={`${ isExpanded ? `block` : `hidden`} md:block md:flex md:items-center w-full md:w-auto`}>
             <ul className="flex flex-grow justify-end flex-wrap items-center">
               <li>
-                <Link to="/signin" className="font-medium text-gray-600 hover:text-gray-900 px-5 py-3 flex items-center transition duration-150 ease-in-out">Sign in</Link>
+                <Link to="/signin" className="font-medium text-gray-600 hover:text-red-900 px-5 py-3 flex items-center transition duration-150 ease-in-out">Sign in</Link>
               </li>
               <li>
-                <Link to="/signup" className="btn-sm text-gray-200 bg-gray-900 hover:bg-gray-800 ml-3">
+                <Link to="/signup" className="btn-sm text-gray-200 bg-red-900 hover:bg-red-800 ml-3">
                   <span>Sign up</span>
                   <svg className="w-3 h-3 fill-current text-gray-400 flex-shrink-0 ml-2 -mr-1" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                     <path d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z" fillRule="nonzero" />
-                  </svg>                  
+                  </svg>
                 </Link>
               </li>
             </ul>
-
           </nav>
-
         </div>
       </div>
     </header>
